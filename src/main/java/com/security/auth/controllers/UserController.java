e4e7507a383cee1644c0f688f984dd36fe521e04
lizamartins <lufernandes.mt@gmail.com>
2024-02-09 03:19:43 -0300

    feat: implements token in controller

--- src/main/java/com/security/auth/controllers/UserController.java
+++ src/main/java/com/security/auth/controllers/UserController.java
@@ -2,18 +2,13 @@ package com.security.auth.controllers;
 
 import com.security.auth.dtos.LoginRecordDTO;
 import com.security.auth.dtos.RegisterRecordDTO;
-import com.security.auth.exceptions.UserNotFoundException;
-import com.security.auth.repositories.UserRepository;
+import com.security.auth.dtos.TokenRecordDTO;
 import com.security.auth.services.UserService;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +21,7 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid LoginRecordDTO loginDTO) {
-        userService.login(loginDTO);
-        return ResponseEntity.status(HttpStatus.OK).body("Login finish");
+        return ResponseEntity.status(HttpStatus.OK).body(new TokenRecordDTO(userService.login(loginDTO)));
     }
 
     @PostMapping("/register")
